7106cfcddefef30651a284402eb154a40102af7f
Ashok verma <ashokverma3779@gmail.com>
2022-08-21 00:51:29 +0530

    Loop

--- 1datatype.cpp
+++ 1datatype.cpp
@@ -1,10 +1,24 @@
-#include <stdio.h>
-
+#include <iostream>
+using namespace std;
 int main()
 {
-    int a = 1;
-    int b = 2;
-    int c = a + b;
-    printf("%d\n", c);
+    int a = 9 ;
+    cout<<"SIZE OF INT" << sizeof(a)<<endl;
+
+    float b = 9.9 ;
+    cout<<"SIZE OF FLOAT" << sizeof(b)<<endl;
+
+    bool c = true ;
+    cout<<"SIZE OF BOOL" << sizeof(c)<<endl;
+
+    char d = 'a' ;
+    cout<<"SIZE OF CHAR" << sizeof(d)<<endl;
+
+    short int e = 9 ;
+    cout<<"SIZE OF SHORT INT" << sizeof(e)<<endl;
+
+    long int f = 9 ;
+    cout<<"SIZE OF LONG INT" << sizeof(f)<<endl;
     return 0;
+    
 }
